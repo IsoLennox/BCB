a677104001dd83d69a285f79dc15b25e6f8133ef
Isobel Lennox <isolennox@gmail.com>
2015-06-20 21:39:48 -0700

    skeleton

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BCB</title>
+</head>
+<body>
+   <?php
+
+
+if(isset($_GET['ashtray'])){
+
+
+//ASHTRAY
+
+
+//DAY VIEW
+
+
+//WEEK VIEW 
+
+
+//MONTH VIEW
+    
+}else{
+
+//ADD ONE
+    
+    
+
+}
+
+?>
+    
+</body>
+</html>
