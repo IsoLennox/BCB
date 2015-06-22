b864dfb6ca905771bc75d72a0b7fe930fc2a62c4
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-22 01:52:24 -0700

    Add check to see if username is taken

--- register.php
+++ register.php
@@ -33,13 +33,25 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <title>BCB New User</title>
     <link rel="stylesheet" href="css/style.css">
     <link href='http://fonts.googleapis.com/css?family=Special+Elite|Carme|Flavors' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </head>
 <body>
   <div id="page" class="container login">
    <h1><label for="New Smoker">New Smoker</label></h1>
    <?php echo message(); ?>
       <form action="#" method="POST">
-       <input type="text" name="username" placeholder="USERNAME">
+       <!-- <p> -->
+       <input type="text" name="username" id="username" placeholder="USERNAME">
+       <script>
+        $("#username").keyup(function(e) {
+          var username = $(this).val();
+          $.post("check_available.php", {"username":username}, function(data) {
+              $("#username_result").html(data);
+            });
+        });
+       </script>
+       <span id="username_result"></span>
+       <!-- </p> -->
        <input type="password" name="password" placeholder="PASSWORD">
        <input type="password" name="confirmpassword" placeholder="CONFIRM PASSWORD">
        <input type="submit" name="login" value="Smoke">
