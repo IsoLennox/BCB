973fcfb8227a73f179ea18abf0d1266067d0da15
Isobel Lennox <isolennox@gmail.com>
2015-06-21 21:12:36 -0700

    updated DB

--- inc/ilennox_bcb.sql
+++ inc/ilennox_bcb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 21, 2015 at 12:32 AM
+-- Generation Time: Jun 21, 2015 at 11:11 PM
 -- Server version: 5.5.42-37.1
 -- PHP Version: 5.4.23
 
@@ -23,23 +23,62 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `friends`
+--
+
+CREATE TABLE IF NOT EXISTS `friends` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `log`
 --
 
 CREATE TABLE IF NOT EXISTS `log` (
   `id` int(11) NOT NULL,
-  `user_id` int(2) NOT NULL,
   `day` int(2) NOT NULL,
   `month` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `year` int(4) NOT NULL,
   `weekday` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `rosemary` int(11) NOT NULL,
-  `isobel` int(11) NOT NULL
+  `isobel` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `log`
+--
+
+INSERT INTO `log` (`id`, `day`, `month`, `year`, `weekday`, `rosemary`, `isobel`, `user_id`) VALUES
+(16, 20, 'June', 2015, 'Saturday', 6, 4, 0),
+(19, 24, 'June', 2013, 'Sunday', 2, 1, 0),
+(18, 21, 'June', 2015, 'Sunday', 4, 4, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(120) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `log`
@@ -48,13 +87,29 @@ ALTER TABLE `log`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `log`
 --
 ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
