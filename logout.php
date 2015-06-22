7c53e1375f0d786bfee8dc611f5f3cd259268a6c
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-21 22:15:17 -0700

    Add logout.php

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+	require_once("inc/session.php");
+	require_once("inc/functions.php");
+
+	// unset($_SESSION["logged_in"]);
+	unset($_SESSION["user_id"]);
+	unset($_SESSION["username"]);
+
+	redirect_to("index.php");
+?>
