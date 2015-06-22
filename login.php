d41a7bf6edda24d35fd4d4efeb5d6d7c723bbe68
Isobel Lennox <isolennox@gmail.com>
2015-06-21 21:19:34 -0700

    updated login/reg forms

--- login.php
+++ login.php
@@ -3,8 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>BCB Login</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+   <h1>Login</h1>
+   <form action="#">
+       <input type="text" name="username" placeholder="USERNAME">
+       <input type="password" name="password" placeholder="PASSWORD">
+       <input type="submit" name="login" value="Smoke">
+   </form>
     
 </body>
 </html>
--- register.php
+++ register.php
@@ -3,8 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>BCB New User</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+   <h1>New Smoker</h1>
+      <form action="#">
+       <input type="text" name="username" placeholder="USERNAME">
+       <input type="password" name="password" placeholder="PASSWORD">
+       <input type="password" name="confirmpassword" placeholder="CONFIRM PASSWORD">
+       <input type="submit" name="login" value="Smoke">
+   </form>
     
 </body>
 </html>
